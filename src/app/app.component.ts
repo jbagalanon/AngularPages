21bff19ca6aef782903c7b52dcdea7cdb91b8339
Joemar Bagalanon <jbagalanon@gmail.com>
2021-06-29 03:04:20 +0800

    wrapping directive as div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -28,9 +29,51 @@ export class AppComponent {
       title: 'Snowy Mountain',
       url: 'https://images.unsplash.com/photo-1523440543111-bf1612d7722d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80',
     },
+    {
+      title: 'At the City',
+      url: 'https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'River Valley',
+      url: 'https://images.unsplash.com/photo-1455577380025-4321f1e1dca7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'At the Falls',
+      url: 'https://images.unsplash.com/photo-1432405972618-c60b0225b8f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'Erupting Volcano',
+      url: 'https://images.unsplash.com/photo-1475776408506-9a5371e7a068?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=654&q=80',
+    },
+    {
+      title: 'Volcanic Mountains',
+      url: 'https://images.unsplash.com/photo-1541845157-a6d2d100c931?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'Island Paradise',
+      url: 'https://images.unsplash.com/photo-1545579133-99bb5ab189bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'Dessert Mountain',
+      url: 'https://images.unsplash.com/photo-1614761064788-ff40eb38a116?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'Hills Side View',
+      url: 'https://images.unsplash.com/photo-1461468611824-46457c0e11fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'Sunset at the Beach',
+      url: 'https://images.unsplash.com/photo-1505845664900-f883fde76fb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
+    {
+      title: 'Snowy Mountain',
+      url: 'https://images.unsplash.com/photo-1523440543111-bf1612d7722d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80',
+    },
+    {
+      title: 'At the City',
+      url: 'https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    },
   ];
 
-  getClass(){
-
-  }
+  getClass() {}
 }
