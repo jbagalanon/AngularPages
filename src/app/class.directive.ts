5d2bf3e5d4b7a2016fe8849d0be754180678e210
Joemar Bagalanon <jbagalanon@gmail.com>
2021-07-02 08:37:40 +0800

    setting of native element like this is not working anymore

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,11 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class ClassDirective {
+  @Input() backgroundColor: string;
+
   constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = 'yellow';
+    setTimeout(() => {
+      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    }, 100);
   }
 }
 
